f5e18178b284fa7c774d96c0887266acb08ddee0
Frederik Hjorth <frederikhjorth@gmail.com>
2017-09-25 14:18:29 +0200

    04panel

---
+++ scripts/04_panel.R
@@ -0,0 +1,38 @@
+setwd("~/GitHub/vkme17")
+
+library(tidyverse)
+library(plm)
+library(stargazer)
+library(clusterSEs)
+library(rms)
+library(multiwayvcov)
+
+#hent data
+lw<-readRDS("data/04_larsen_wide.rds")
+
+#konverter til long form
+ll<-lw %>% 
+  gather(v,value,hp_1yr.2005:unemprate.2015) %>% 
+  separate(v,into=c("var","year"),sep="\\.") %>% 
+  arrange(valgstedid) %>% 
+  spread(var,value) %>% 
+  mutate(yrfac=factor(year),vstedfac=factor(valgstedid))
+
+#baggrund: https://stackoverflow.com/a/24151902/3082968
+
+#pooled OLS
+pols<-lm(incsupport~hp_1yr,data=ll)
+summary(pols)
+
+#panel model m. LSDV
+fe_lsdv<-lm(incsupport~hp_1yr+yrfac+vstedfac,data=ll)
+summary(fe_lsdv)
+
+#cluster-robuste se's m multiwayvcov
+fe_cvcov<-coeftest(fe_lsdv,vcov=cluster.vcov(fe_lsdv,ll$vstedfac))
+fe_cvcovses<-fe_cvcov[,2]
+
+#alle estimater i én tabel
+stargazer(pols,fe_lsdv,fe_lsdv,type="text",omit=c("yrfac","vstedfac"),
+          se=list(NULL,NULL,fe_cvcovses))
+
--- scripts/98_larsen_longtowide.R
+++ scripts/98_larsen_longtowide.R
@@ -1,4 +1,4 @@
-setwd("~/GitHub/vkme17/scripts")
+setwd("~/GitHub/vkme17")
 
 library(haven)
 library(tidyverse)
@@ -6,15 +6,15 @@ library(reshape2)
 library(data.table)
 
 #import long form data
-ll<-read_dta("../../housing/data/raplidata.dta") %>% 
+ll<-read_dta("../housing/data/raplidata.dta") %>% 
   select(year,valgstedid,hp_1yr,incsupport,unemprate) %>% 
   filter(!is.na(year) & !is.na(valgstedid))
 
 #long to wide
 lw<-ll %>% 
   as.data.frame() %>% 
-  reshape(.,idvar="valgstedid",timevar="year",direction="wide",sep="_") %>% 
+  reshape(.,idvar="valgstedid",timevar="year",direction="wide",sep=".") %>% 
   as_tibble()
 
 #save as RDS
-saveRDS(lw,"../data/04_larsen_wide.rds")
+saveRDS(lw,"data/04_larsen_wide.rds")
